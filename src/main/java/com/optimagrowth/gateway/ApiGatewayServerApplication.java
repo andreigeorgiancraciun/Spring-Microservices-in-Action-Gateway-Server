0a90c2603da2f3fd09e704f2e6eb1717f359139c
andrei-georgian.craciun <andreigeorgiancraciun@yahoo.com>
2023-10-23 22:36:24 +0300

    Chapter 11 - Distributed tracing with Zipkin and ELK

--- src/main/java/com/optimagrowth/gateway/ApiGatewayServerApplication.java
+++ src/main/java/com/optimagrowth/gateway/ApiGatewayServerApplication.java
@@ -2,11 +2,13 @@ package com.optimagrowth.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class ApiGatewayServerApplication {
 
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(ApiGatewayServerApplication.class, args);
     }
 
